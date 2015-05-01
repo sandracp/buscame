339e797a152291cf4154154100fddc0451506fa6
Sandra Pena <scp170@gmail.com>
2015-05-01 16:14:20 -0300

    Agrego enunciado en carpeta docs. Agrego la clase Result e index.jsp para mostrar resultados. Borro index.html

--- src/java/org/pena/sandra/buscame/web/servlet/SearchServlet.java
+++ src/java/org/pena/sandra/buscame/web/servlet/SearchServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.pena.sandra.web.servlet;
+package org.pena.sandra.buscame.web.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
